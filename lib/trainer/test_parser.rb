73af1bf2eb50e9d5dab0c39f25c64874ddc8538a
KrauseFx <KrauseFx@gmail.com>
2016-08-02 21:53:16 -0700

    Remove debug information

--- lib/trainer/test_parser.rb
+++ lib/trainer/test_parser.rb
@@ -26,9 +26,6 @@ module Trainer
           to_path = path.gsub(".plist", config[:extension])
         end
 
-        puts "#{path}: "
-        puts File.read(path)
-        puts "-"
         File.write(to_path, Trainer::TestParser.new(path).to_junit)
         puts "Successfully generated '#{to_path}'"
         to_path
